16297b2404220187c32d7b763af5966b64b3fca5
leofdss <leonardo.f.desouzasilva@gmail.com>
2019-04-16 15:17:48 -0300

    create file explorer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatCardModule } from '@angular/material/card';
+import { FileExplorerComponent } from './file-explorer/file-explorer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FileExplorerComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    FlexLayoutModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
